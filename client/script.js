7c99f193643af6eab14e956fce0b8a31c98f783c
Siddharth verma <990.siddharthverma@gmail.com>
2023-01-11 23:51:53 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -2,7 +2,7 @@ import bot from './assets/bot.svg'
 import user from './assets/user.svg'
 
 const form = document.querySelector('form')
-const chatContainer = document.querySelector('#chat-container')
+const chatContainer = document.querySelector('#chat_container')
 
 let loadInterval
 
@@ -10,9 +10,11 @@ function loader(element) {
     element.textContent = ''
 
     loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
         element.textContent += '.';
 
-        if (element.textContent === '.....') {
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === '....') {
             element.textContent = '';
         }
     }, 300);
@@ -31,7 +33,9 @@ function typeText(element, text) {
     }, 20)
 }
 
-
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
 function generateUniqueId() {
     const timestamp = Date.now();
     const randomNumber = Math.random();
@@ -59,58 +63,58 @@ function chatStripe(isAi, value, uniqueId) {
 }
 
 const handleSubmit = async (e) => {
-  e.preventDefault();
+    e.preventDefault()
 
-  const data = new FormData(form)
+    const data = new FormData(form)
 
-  // user's chatstripe
-  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+    // user's chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
 
-  form.reset()
+    // to clear the textarea input 
+    form.reset()
 
-  // bot's chatstripe
-  const uniqueId = generateUniqueId()
-  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+    // bot's chatstripe
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
 
-  // to focus scroll to the bottom 
-  chatContainer.scrollTop = chatContainer.scrollHeight;
+    // to focus scroll to the bottom 
+    chatContainer.scrollTop = chatContainer.scrollHeight;
 
+    // specific message div 
+    const messageDiv = document.getElementById(uniqueId)
 
-  const messageDiv = document.getElementById(uniqueId);
-  loader(messageDiv);
+    // messageDiv.innerHTML = "..."
+    loader(messageDiv)
 
-  const response = await fetch('https://codex-ai-pkus.onrender.com', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-        prompt: data.get('prompt')
+    const response = await fetch('https://codex-ai-pkus.onrender.com/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
     })
-  })
-  clearInterval(loadInterval);
-  messageDiv.innerHTML = '';
 
-  if(response.ok) {
-    const data = await response.json();
-    const parsedData = data.bot.trim();
+    clearInterval(loadInterval)
+    messageDiv.innerHTML = " "
 
-    typeText(messageDiv, parsedData);
-  } else {
-    const err = await response.text();
+    if (response.ok) {
+        const data = await response.json();
+        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
 
-    messageDiv.innerHTML = "Something's Went Wrong";
+        typeText(messageDiv, parsedData)
+    } else {
+        const err = await response.text()
 
-    alert(err);
-  }
+        messageDiv.innerHTML = "Something went wrong"
+        alert(err)
+    }
 }
 
-form.addEventListener('submit', handleSubmit);
+form.addEventListener('submit', handleSubmit)
 form.addEventListener('keyup', (e) => {
-  if(e.keyCode === 13) {
-    handleSubmit(e);
-  }
+    if (e.keyCode === 13) {
+        handleSubmit(e)
+    }
 })
-
-
-
